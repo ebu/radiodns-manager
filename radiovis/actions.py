c131da9b35121cf6767994d07a4b2b656f94a0ef
Maximilien Cuony <cuony@ebu.ch>
2013-08-06 16:21:12 +0200

    RadioVIS

---
+++ radiovis/actions.py
@@ -0,0 +1,182 @@
+# -*- coding: utf-8 -*-
+
+# Utils
+from utils import action, only_orga_member_user, only_orga_admin_user, PlugItRedirect, json_only, PlugItSendFile
+
+from models import db, Station, Channel, Picture
+
+import urlparse
+
+import os
+import sys
+
+from werkzeug import secure_filename
+
+from PIL import Image
+import imghdr
+
+
+@action(route="/radiovis/gallery/", template="radiovis/gallery/home.html")
+@only_orga_member_user()
+def radiovis_gallery_home(request):
+    """Show the list of pictures."""
+
+    list = []
+
+    for elem in Picture.query.filter_by(orga = int(request.args.get('ebuio_orgapk'))).order_by(Picture.name).all():
+        list.append(elem.json)
+
+    saved = request.args.get('saved') == 'yes'
+    deleted = request.args.get('deleted') == 'yes'
+    
+    return {'list': list, 'saved': saved, 'deleted': deleted}
+
+
+@action(route="/radiovis/gallery/edit/<id>", template="radiovis/gallery/edit.html", methods=['POST', 'GET'])
+@only_orga_admin_user()
+def radiovis_gallery_edit(request, id):
+    """Edit a channel."""
+
+    object = None
+    errors = []
+
+    if id != '-':
+        object =  Picture.query.filter_by(orga=int(request.args.get('ebuio_orgapk') or request.form.get('ebuio_orgapk')), id=int(id)).first()
+
+    if request.method == 'POST':
+
+        if not object:
+            object = Picture(int(request.form.get('ebuio_orgapk')))
+
+        object.name = request.form.get('name')
+        object.radiotext = request.form.get('radiotext')
+        object.radiolink = request.form.get('radiolink')
+
+        def add_unique_postfix(fn):
+            """__source__ = 'http://code.activestate.com/recipes/577200-make-unique-file-name/'"""
+            if not os.path.exists(fn):
+                return fn
+
+            path, name = os.path.split(fn)
+            name, ext = os.path.splitext(name)
+
+            make_fn = lambda i: os.path.join(path, '%s(%d)%s' % (name, i, ext))
+
+            for i in xrange(2, sys.maxint):
+                uni_fn = make_fn(i)
+                if not os.path.exists(uni_fn):
+                    return uni_fn
+
+            return None
+
+        file = request.files['file']
+        if file:
+            filename = secure_filename(file.filename)
+            full_path = add_unique_postfix('media/uploads/radiovis/gallery/' + filename)
+            file.save(full_path)
+            if object.filename:
+                try:
+                    os.unlink(object.filename)
+                except:
+                    pass
+            object.filename = full_path
+
+
+
+        # Check errors
+        if object.name == '':
+            errors.append("Please set a name")
+
+        if object.radiotext == '':
+            errors.append("Please set a text")
+
+        if object.radiolink == '':
+            errors.append("Please set a link")
+
+        if object.filename == '' or object.filename is None:
+            errors.append("Please upload an image")
+        else:
+
+            if imghdr.what(object.filename) not in ['jpeg', 'png']:
+                errors.append("Image is not an png or jpeg image")
+                os.unlink(object.filename)
+                object.filename = None
+            else:
+                im = Image.open(object.filename)
+                if im.size != (320, 240):
+                    errors.append("Image must be 320x240")
+                    del im
+                    os.unlink(object.filename)
+                    object.filename = None
+
+        pieces = urlparse.urlparse(object.radiolink)
+
+        if not pieces.scheme in ['http', 'https', 'ftp']:
+            errors.append("The link is not valid")
+
+
+        # If no errors, save
+        if not errors:
+            
+            if not object.id:
+                db.session.add(object)
+
+            db.session.commit()
+
+            return PlugItRedirect('radiovis/gallery/?saved=yes')
+
+    if object:
+        object = object.json
+       
+    return {'object': object, 'errors': errors}
+
+@action(route="/radiovis/gallery/delete/<id>")
+@json_only()
+@only_orga_admin_user()
+def radiovis_gallery_delete(request, id):
+    """Delete a picture."""
+
+    object = Picture.query.filter_by(orga=int(request.args.get('ebuio_orgapk')), id=int(id)).first()
+
+    db.session.delete(object)
+    db.session.commit()
+
+    return PlugItRedirect('radiovis/gallery/?deleted=yes')
+
+@action(route="/radiovis/channels/", template="radiovis/channels/home.html")
+@only_orga_member_user()
+def radiovis_channels_home(request):
+    """Show the list of channels to edit current picture."""
+
+    list = []
+
+    for elem in Channel.query.join(Station).filter(Station.orga==int(request.args.get('ebuio_orgapk'))).order_by(Channel.name).all():
+        list.append(elem.json)
+
+    pictures = []
+
+    for elem in Picture.query.filter_by(orga = int(request.args.get('ebuio_orgapk'))).order_by(Picture.name).all():
+        pictures.append(elem.json)
+
+
+    return {'list': list, 'pictures': pictures}
+
+@action(route="/radiovis/channels/set/<id>/<pictureid>")
+@only_orga_admin_user()
+@json_only()
+def radiovis_channels_set(request, id, pictureid):
+    """Set a default value for a channel"""
+
+   
+    object = Channel.query.join(Station).filter(Channel.id == int(id), Station.orga==int(request.args.get('ebuio_orgapk'))).first()
+    
+    picture = Picture.query.filter_by(orga=int(request.args.get('ebuio_orgapk')), id=int(pictureid)).first()
+    
+    if picture:
+        object.default_picture_id = picture.id
+    else:
+        object.default_picture_id = None
+
+    db.session.commit()
+
+    return {}
