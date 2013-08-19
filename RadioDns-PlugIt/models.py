12ebcb7c4bdea26c46eb8cd077625ad4b4ed4484
Maximilien Cuony <cuony@ebu.ch>
2013-08-19 18:01:24 +0200

    RadioEPG: XML

--- RadioDns-PlugIt/models.py
+++ RadioDns-PlugIt/models.py
@@ -279,3 +279,28 @@ class Schedule(db.Model):
     def seconds_from_base(self):
         """The number, in seconds of start, based on monday 00:00"""
         return self.day * 24 * 60 * 60 + self.start_hour * 60 * 60 + self.start_minute * 60
+
+    @property
+    def duration(self):
+
+        return 'PT' + str(int(self.length/60)) + 'H' + str(self.length%60) + 'M'
+
+
+    @property
+    def start_time(self):
+        """Return the start time, assuming start_date has been set as a reference"""
+        import datetime
+        timetime_format = '%Y%m%dT%H%M%S'
+
+        if not hasattr(self, 'start_date'):
+            return ''
+
+        return (self.start_date + datetime.timedelta(days=self.day, hours=self.start_hour, minutes=self.start_minute)).strftime(timetime_format)
+
+    @property
+    def json_show(self):
+        return self.show.json
+
+    @property
+    def json(self):
+        return to_json(self, self.__class__, ['json_show', 'start_time', 'duration'])
