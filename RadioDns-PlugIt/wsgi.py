089f890dd31778a5ffa052da1f1775435bf01664
Maximilien Cuony <maximilien@theglu.org>
2013-11-27 11:42:09 +0100

    New wsgi

--- RadioDns-PlugIt/wsgi.py
+++ RadioDns-PlugIt/wsgi.py
@@ -1,4 +1,8 @@
 import os
 os.chdir('/home/ubuntu/gitrepo-plugit/RadioDns-PlugIt')
 
+from plugit import routes
+import actions
 from plugit import app as application
+
+routes.load_routes(application, actions)
