a0c09f70c28206f3bbb673b77ec07dd64e6ed9ff
Maximilien Cuony <maximilien@theglu.org>
2014-02-11 14:12:16 +0100

    Real 404

--- RadioDns-PlugIt/plugit/routes.py
+++ RadioDns-PlugIt/plugit/routes.py
@@ -48,7 +48,6 @@ def load_routes(app,actions):
         base_type = path.split('/')[0]
         topiced_path = '/topic/' + path
 
-
         station = None
 
         for channel in Channel.query.filter(Channel.type_id == base_type).all():
@@ -63,14 +62,13 @@ def load_routes(app,actions):
 
             today = datetime.date.today()
             start_date = datetime.datetime.combine(today - datetime.timedelta(days=today.weekday()), datetime.time())
-            end_date = start_date + datetime.timedelta(days=6,hours=23,minutes=59,seconds=59)
+            end_date = start_date + datetime.timedelta(days=6, hours=23, minutes=59, seconds=59)
 
 
             # Filter by date
             date_to_filter = datetime.datetime.strptime(str(date), "%Y%m%d").date()
             real_start_date = datetime.datetime.combine(date_to_filter, datetime.time())
-            real_end_date = start_date + datetime.timedelta(days=6,hours=23,minutes=59,seconds=59)
-
+            real_end_date = start_date + datetime.timedelta(days=6, hours=23, minutes=59, seconds=59)
 
             list = []
 
@@ -80,31 +78,24 @@ def load_routes(app,actions):
                 if elem.date_of_start_time.date() == date_to_filter:
                     list.append(elem.json)
 
-            
-
             return Response(render_template('radioepg/schedule/xml.html', schedules=list, start_time=real_start_date.strftime(time_format), end_time=real_end_date.strftime(time_format), creation_time=datetime.datetime.now().strftime(time_format)), mimetype='text/xml')
 
-        return 'Station not found'
-
-
-
-    
+        abort(404)
+        #return 'Station not found'
 
     @app.route(PI_BASE_URL + "ping")
     def ping():
         """The ping method: Just return the data provided"""
-    
+
         if not check_ip(request):
             return
-    
+
         return jsonify(data=request.args.get('data', ''))
-    
-    
+
     @app.route(PI_BASE_URL + "version")
     def version():
         """The version method: Return current information about the version"""
         return jsonify(result='Ok', version=PI_API_VERSION, protocol=PI_API_NAME)
-
 
     @app.route(PI_BASE_URL + "mail", methods=['POST'])
     def mail():
@@ -130,9 +121,9 @@ def load_routes(app,actions):
             db.session.commit()
 
             post.send_mail()
-            
+
         return jsonify(result='Ok')
-    
+
     # Register the 3 URLs (meta, template, action) for each actions
     # We test for each element in the module actions if it's an action
     # (added by the decorator in utils)
@@ -140,17 +131,17 @@ def load_routes(app,actions):
         obj = getattr(actions, act)
         if hasattr(obj, 'pi_api_action') and obj.pi_api_action:
             # We found an action and we can now add it to our routes
-            
+
             # Meta
             app.add_url_rule(
                 PI_BASE_URL + 'meta' + obj.pi_api_route,
                 view_func=MetaView.as_view('meta_' + act, action=obj))
-    
+
             # Template
             app.add_url_rule(
                 PI_BASE_URL + 'template' + obj.pi_api_route,
                 view_func=TemplateView.as_view('template_' + act, action=obj))
-    
+
             # Action
             app.add_url_rule(
                 PI_BASE_URL + 'action' + obj.pi_api_route,
