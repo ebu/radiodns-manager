64fc326a6e59b65bd35c2ea8e2fcd914784ffca9
Maximilien Cuony <maximilien@theglu.org>
2014-03-25 11:46:36 +0100

    Sentry

--- RadioDns-PlugIt/plugit/__init__.py
+++ RadioDns-PlugIt/plugit/__init__.py
@@ -8,10 +8,17 @@ import routes
 
 from params import PI_BASE_URL
 
-app = Flask("sample-project", static_folder='media', static_url_path=PI_BASE_URL+'media')
+app = Flask("sample-project", static_folder='media', static_url_path=PI_BASE_URL + 'media')
 # For SqlAlchemy
 app.config['SQLALCHEMY_DATABASE_URI'] = config.SQLALCHEMY_URL
+
+if config.SENTRY_DSN:
+    from raven.contrib.flask import Sentry
+    app.config['SENTRY_DSN'] = config.SENTRY_DSN
+    sentry = Sentry(app)
+
 db = SQLAlchemy(app)
+
 
 def load_actions(act_mod):
     routes.load_routes(app, act_mod)
