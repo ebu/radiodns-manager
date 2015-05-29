5a33d5af5acc0e20e09677629cbcf100266a2da0
mhabegger <mh@swissmediapartners.ch>
2015-05-29 10:32:00 +0200

    Correct caching issue on XSI

--- RadioDns-PlugIt/plugit/__init__.py
+++ RadioDns-PlugIt/plugit/__init__.py
@@ -28,8 +28,14 @@ if config.SENTRY_DSN:
 
 db = SQLAlchemy(app)
 
+def make_hostname_cache_key(*args, **kwargs):
+    """Generates a cachekey containing the hostname"""
+    hostname = request.host
+    args = str(hash(frozenset(request.args.items())))
+    return (hostname + '_' +args).encode('utf-8')
+
 @app.route('/radiodns/epg/XSI.xml')
-@app.cache.cached(timeout=120)
+@app.cache.cached(timeout=120, key_prefix='XSI1_')
 def epg_xml():
     """Special call for EPG xml"""
     # Specified by 3.1.1 /radiodns/epg/XSI.xml
@@ -59,65 +65,70 @@ def epg_xml():
             if provider:
                 # We have a station based query
                 sp = ServiceProvider.query.filter_by(codops=provider).order_by(ServiceProvider.codops).first()
-                stations = Station.query.filter_by(service_provider_id=sp.id, radioepg_enabled=True) #, fqdn_prefix=station)
+                if sp:
+                    stations = Station.query.filter_by(service_provider_id=sp.id, radioepg_enabled=True) #, fqdn_prefix=station)
 
             if station:
                 # TODO FIX Filtering by property does not workin in SQLAlchemy, thus using regular python to filter
                 stations = filter(lambda x: x.fqdn_prefix == station, stations)
+
         else:
+            sp = ServiceProvider.query.filter_by(codops="EBU").order_by(ServiceProvider.codops).first()
             stations = Station.query.filter_by(radioepg_enabled=True)
 
         if not sp:
-            sp = ServiceProvider.query.filter_by(codops="EBU").order_by(ServiceProvider.codops).first()
+            abort(404)
 
         list = []
 
-        for elem in stations:
+        if stations:
+            for elem in stations:
 
-            entries = []
+                entries = []
 
-            # Channels
-            for elem2 in elem.channels.order_by(Channel.name).all():
-                if elem2.servicefollowingentry.active:
-                    entries.append(elem2.servicefollowingentry.json)
+                # Channels
+                for elem2 in elem.channels.order_by(Channel.name).all():
+                    if elem2.servicefollowingentry.active:
+                        entries.append(elem2.servicefollowingentry.json)
 
-                    if elem2.type_id == 'fm':  # For FM, also add with the country code
-                        try:
-                            data2 = elem2.servicefollowingentry.json
+                        if elem2.type_id == 'fm':  # For FM, also add with the country code
+                            try:
+                                data2 = elem2.servicefollowingentry.json
 
-                            # Split the URI
-                            uri_dp = data2['uri'].split(':', 2)
-                            uri_p = uri_dp[1].split('.')
+                                # Split the URI
+                                uri_dp = data2['uri'].split(':', 2)
+                                uri_p = uri_dp[1].split('.')
 
-                            pi_code = uri_p[0]
+                                pi_code = uri_p[0]
 
-                            # Get the ISO code form the picode
-                            ecc = Ecc.query.filter_by(pi=pi_code[0].upper(), ecc=pi_code[1:].upper()).first()
+                                # Get the ISO code form the picode
+                                ecc = Ecc.query.filter_by(pi=pi_code[0].upper(), ecc=pi_code[1:].upper()).first()
 
-                            uri_p[0] = ecc.iso.lower()
+                                uri_p[0] = ecc.iso.lower()
 
-                            # Update the new URL
-                            data2['uri'] = uri_dp[0] + ':' + '.'.join(uri_p)
+                                # Update the new URL
+                                data2['uri'] = uri_dp[0] + ':' + '.'.join(uri_p)
 
-                            # Add the service
-                            entries.append(data2)
+                                # Add the service
+                                entries.append(data2)
 
-                        except:
-                            pass
+                            except:
+                                pass
 
-            # Custom entries
-            for elem2 in elem.servicefollowingentries.order_by(GenericServiceFollowingEntry.channel_uri).all():
-                if elem2.active:
-                    entries.append(elem2.json)
+                # Custom entries
+                for elem2 in elem.servicefollowingentries.order_by(GenericServiceFollowingEntry.channel_uri).all():
+                    if elem2.active:
+                        entries.append(elem2.json)
 
-            if entries:
-                list.append([elem.json, entries])
+                if entries:
+                    list.append([elem.json, entries])
+
         return Response(render_template('radioepg/servicefollowing/xml.html', stations=list, service_provider=sp,
                                         creation_time=datetime.datetime.now().strftime(time_format)), mimetype='text/xml')
 
     # Else
     abort(404)
-
+epg_xml.make_cache_key = make_hostname_cache_key
 
 @app.route('/radiodns/logo/<int:id>/<int:w>/<int:h>/logo.png')
 @app.cache.cached(timeout=120)
